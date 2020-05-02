a112f077bd8eecd1f57b9eaa8bafc1c05ce7f88b
dushaoh <dushaoh@cn.ibm.com>
2020-05-02 15:40:58 +0800

    First commit

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ResponseResult } from '../models/ResponseResult';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+
+  //private baseUrl = 'http://localhost:8080';
+  private baseUrl = 'http://localhost:3000';
+  //private baseUrl = '/api/v1';
+
+  constructor(private http: HttpClient) { }
+
+  get(url: string) {
+    return this.http.get(this.composeUrl(url));
+  }
+
+  download(url: string) {
+    return this.http.get(this.composeUrl(url), {responseType: 'blob'});
+  }
+
+  post(url: string, body: any) {
+    //return this.http.get(this.composeUrl(url), body);
+    return this.http.get(this.composeUrl(url));
+  }
+
+  put(url: string, body: any) {
+    return this.http.put(this.composeUrl(url), body);
+  }
+
+  private composeUrl(url: string): string {
+    return `${this.baseUrl}${url}`;
+    //return url;
+  }
+}
